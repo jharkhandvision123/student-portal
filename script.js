ab55c08b5b24c1d22fa2029baf15ea36a99d80fd
jharkhandvision123 <wingsgroup2026@gmail.com>
2026-07-29 22:57:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -201,3 +201,26 @@ if (searchBox) {
     });
 
 }
+// Image Slider
+
+const slides = document.querySelectorAll(".slide");
+
+if (slides.length > 0) {
+
+    let currentSlide = 0;
+
+    setInterval(() => {
+
+        slides[currentSlide].classList.remove("active");
+
+        currentSlide++;
+
+        if (currentSlide >= slides.length) {
+            currentSlide = 0;
+        }
+
+        slides[currentSlide].classList.add("active");
+
+    }, 3000);
+
+}
