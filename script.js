62114506c0da502d108ecef209c6ec27314b4004
jharkhandvision123 <wingsgroup2026@gmail.com>
2026-07-29 15:45:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,45 @@
- 
+ /* CSS File */).
+function function checkAnswer() {
+
+    let options = document.getElementsByName("q1");
+    let result = document.getElementById("result");
+
+    if (options[2].checked) {
+        result.innerHTML = "✅ Correct Answer";
+        result.style.color = "green";
+    } else {
+        result.innerHTML = "❌ Wrong Answer";
+        result.style.color = "red";
+    }
+
+} {
+
+    let options = document.getElementsByName("q1");
+
+    if (options[2].checked) {
+
+        alert("✅ Correct Answer");
+
+    } else {
+document.getElementById("nextBtn").style.display = "inline-block";
+        alert("❌ Wrong Answer");
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
