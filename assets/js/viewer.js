c2f781782d451db6deda0b3cd4b896526411e67a
jharkhandvision123 <wingsgroup2026@gmail.com>
2026-08-03 17:38:35 +0530

    Create viewer.js

---
+++ assets/js/viewer.js
@@ -0,0 +1,6 @@
+document.getElementById("viewerTitle").innerHTML = "Universal Viewer";
+
+document.getElementById("contentTitle").innerHTML = "Viewer Ready";
+
+document.getElementById("contentArea").innerHTML =
+"<h3>Dynamic System Successfully Started</h3>";
